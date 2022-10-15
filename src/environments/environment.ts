aaa840f4e85b1d33a6d63f14e05f6118fb1829d2
Anastasiia Erokhina <erokhinaanastasiia@gmail.com>
2022-10-15 16:24:35 +0300

    created movies application

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '64857fbdb2daf8bc6872bb0498108923',
+  baseUrl: 'https://api.themoviedb.org/3',
+  images: 'http://image.tmdb.org/t/p',
 };
 
 /*
